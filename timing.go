6443d9aa4d64b405521d9687636fdfe4b9104884
Arstd <shangxuejin@gmail.com>
2016-11-21 16:26:34 +0800

    generate atomic next id

--- timing.go
+++ timing.go
@@ -3,6 +3,7 @@ package timing
 import (
 	"container/heap"
 	"fmt"
+	"sync/atomic"
 	"time"
 )
 
@@ -26,30 +27,36 @@ var (
 	inited = make(chan struct{})
 )
 
+var next = func() func() uint32 {
+	var id uint32
+	return func() uint32 {
+		return atomic.AddUint32(&id, 1)
+	}
+}()
+
 // Init 初始化定时器，并启动
 func Init(items ...*Item) {
 	q := make(Queue, len(items), 1024)
 	for i, item := range items {
-		item.Id = uint32(i)
+		item.Id = next()
 		q[i] = item
 		PersistFunc(item)
 	}
 	go start(q)
 }
 
-// Add 用来设置定时项，必须先 Init，否则 Add 会一直等待 Init
+// Add 用来添加定时项并设置ID，必须先 Init，否则 Add 会一直等待 Init
 func Add(items ...*Item) {
 	<-inited
 
 	for _, item := range items {
+		item.Id = next()
 		stage <- item
 	}
 }
 
 func start(q Queue) {
 	heap.Init(&q)
-
-	var next = uint32(q.Len())
 
 	var min *Item
 	var timer = time.NewTimer(24 * time.Hour)
@@ -58,14 +65,11 @@ func start(q Queue) {
 		min = heap.Pop(&q).(*Item)
 		timer = time.NewTimer(time.Unix(int64(min.When), 0).Sub(time.Now()))
 	}
-	close(inited)
 
+	close(inited)
 	for {
 		select {
 		case item := <-stage:
-			item.Id = next
-			next++
-
 			if min == nil {
 				// do nothing
 			} else if item.When < min.When {
